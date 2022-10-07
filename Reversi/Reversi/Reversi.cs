5455d0edd865c6aafbd2218b1ca44e5f69f7c666
hugokrul <hugo.krul@gmail.com>
2022-10-07 11:57:14 +0200

    ProjectAanmaken

---
+++ Reversi/Reversi/Reversi.cs
@@ -0,0 +1,8 @@
+using System.Drawing;
+using System.Windows.Forms;
+
+Form scherm = new Form();
+scherm.Text = "Reversi";
+scherm.BackColor = Color.LightYellow;
+scherm.ClientSize = new Size(200, 100);
+Application.Run(scherm);
